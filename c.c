f1e387f422fd55acc74b2ca2d57154a552b109a0
uvraag <uvraag@outlook.com>
2018-04-03 12:58:08 -0700

    c

---
+++ c.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+void main() {
+   int choice;
+   char ch='n';
+   clrscr();
+   do {
+      printf("1) Student Information\n");
+      printf("2) Course Material Despatch Status\n");
+      printf("3) Fee Status (Paid or Due for payment)\n");
+      printf("4) Time Table for Theory Counselling\n");
+      printf("5) \n");
+      printf("6) \n");
+      printf("7) \n");
+      printf("8) \n");
+      printf("9) \n");
+   } while(ch=='y');
+}
