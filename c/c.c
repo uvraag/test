e6630caccac2ac426b661e7d6877a444e2d43303
uvraag <uvraag@outlook.com>
2018-04-09 06:05:13 -0700

    main complete

--- c/c.c
+++ c/c.c
@@ -9,10 +9,24 @@ void main() {
       printf("2) Course Material Despatch Status\n");
       printf("3) Fee Status (Paid or Due for payment)\n");
       printf("4) Time Table for Theory Counselling\n");
-      printf("5) \n");
-      printf("6) \n");
-      printf("7) \n");
-      printf("8) \n");
-      printf("9) \n");
+      printf("5) Time Table for Practical Counselling\n");
+      printf("6) Assignment Submission Schedules\n");
+      printf("7) Change of the Correspondence Address\n");
+      printf("8) General Queries\n");
+      printf("9) Quit\n");
+      printf("Enter your choice : \n");
+      scanf("%d", &choice);
+      switch (choice) {
+         case 1:  studentInfo(); break;
+         case 2:  despatch(); break;
+         case 3:  fee(); break;
+         case 4:  theory(); break;
+         case 5:  practical(); break;
+         case 6:  assignment(); break;
+         case 7:  changeAddress(); break;
+         case 8:  queries(); break;
+         case 9:  exit(0); break;
+         default: ch='y'; clrscr();
+      }
    } while(ch=='y');
 }
