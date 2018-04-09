8b00226a46db3879a93c842ddcf53d04d9f31d76
uvraag <uvraag@outlook.com>
2018-04-09 06:32:52 -0700

    add studentInfo()

--- c/c.c
+++ c/c.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <conio.h>
+#include <stdlib.h>
+void studentInfo();
+void despatch();
+void fee();
+void theory();
+void practical();
+void assignment();
+void changeAddress();
+void queries();
 void main() {
    int choice;
    char ch='n';
@@ -13,7 +22,7 @@ void main() {
       printf("6) Assignment Submission Schedules\n");
       printf("7) Change of the Correspondence Address\n");
       printf("8) General Queries\n");
-      printf("9) Quit\n");
+      printf("9) Quit\n\n");
       printf("Enter your choice : \n");
       scanf("%d", &choice);
       switch (choice) {
@@ -29,4 +38,52 @@ void main() {
          default: ch='y'; clrscr();
       }
    } while(ch=='y');
+}
+
+void studentInfo() {
+   long en;
+   int yos, fee;
+   char nop[5],
+      batch[7],
+      dur[20],
+      nosc[30],
+      norc[30];
+   printf("\nEnter Enrollment Number : \n", );
+   scanf("%lf", &en);
+   switch (en) {
+      case 176000100:   yos=1;
+         strcpy(nop, "BCA");
+         strcpy(batch, "B2T1");
+         strcpy(dur, "5 years only");
+         strcpy(nosc, "TSL Institute");
+         strcpy(norc, "Ramphal Chowk");
+         fee=1000;
+         break;
+      case 176000101:   yos=2;
+         strcpy(nop, "BCA");
+         strcpy(batch, "B2T1");
+         strcpy(dur, "4 years only");
+         strcpy(nosc, "TSL Institute");
+         strcpy(norc, "Tilak Nagar");
+         fee=1500;
+         break;
+      case 176000102:   yos=3;
+         strcpy(nop, "BCA");
+         strcpy(batch, "B2T2");
+         strcpy(dur, "3 years only");
+         strcpy(nosc, "KML Institute");
+         strcpy(norc, "Dwarka Sector 9");
+         fee=2000;
+         break;
+      default: printf("You Entered Wrong enrollment number\nTry Again\n");
+         exit(0);
+   }
+
+   printf("Year of Study is %d\n", yos);
+   printf("Programme name is %s\n", nop);
+   printf("Your batch is %s\n", batch);
+   printf("Duration : %s\n", dur);
+   printf("Your Study Centre is %s\n", nosc);
+   printf("Your Regional Centre is %s\n", nosc);
+   printf("Due balance is %d\n", fee);
 }
