c487c5bea33b61953adc66a5eed8db3f88e952e7
uvraag <uvraag@outlook.com>
2018-04-10 05:55:49 -0700

    short program

---
+++ js/duplidate_array_ele_removed.js
@@ -0,0 +1,3 @@
+arr = arr.filter( function ( item, index, inputArray ) {
+   return inputArray.indexOf( item ) == index;
+});
---
+++ js/shuffle_array.js
@@ -0,0 +1,13 @@
+function shuffle( array ) {
+   var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+   while ( currentIndex !== 0 ) {
+      randomIndex Math.floor( Math.random() * currentIndex );
+      currentIndex--;
+      temporaryValue = array[ currentIndex ];
+      array[ currentIndex ] = array[ randomIndex ];
+      array[ randomIndex ] = temporaryValue;
+   }
+   return array;
+}
